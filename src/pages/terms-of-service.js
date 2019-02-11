065811940d4f3f789043056d142307905c24d755
Ivan Hueso <ivanhueso@gmail.com>
2019-02-11 17:33:32 -0300

    Fix link on Term of Services

--- src/pages/terms-of-service.js
+++ src/pages/terms-of-service.js
@@ -16,12 +16,12 @@ const TermsOfService = () => (
     <h3>1. Terms</h3>
     <p>
       By accessing the website at{' '}
-      <Link to="/privacy-policy">https://www.getbestoffers.net</Link>, you are
-      agreeing to be bound by these terms of service, all applicable laws and
-      regulations, and agree that you are responsible for compliance with any
-      applicable local laws. If you do not agree with any of these terms, you
-      are prohibited from using or accessing this site. The materials contained
-      in this website are protected by applicable copyright and trademark law.
+      <Link to="/">https://www.getbestoffers.net</Link>, you are agreeing to be
+      bound by these terms of service, all applicable laws and regulations, and
+      agree that you are responsible for compliance with any applicable local
+      laws. If you do not agree with any of these terms, you are prohibited from
+      using or accessing this site. The materials contained in this website are
+      protected by applicable copyright and trademark law.
     </p>
     <h3>2. Use License</h3>
     <ol type="a">
